97b09ea2895bcc4fa3ede2ae545c2b344f2a0e1f
Sean Culliton <smculliton@gmail.com>
2022-11-10 21:36:13 -0700

    Finish merchant discounts show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'the bulk discounts show page' do 
+  before(:each) do 
+    @edibles = Merchant.create!(name: 'Edible Arrangements')
+
+    @discount1 = @edibles.bulk_discounts.create!(percentage: 20, threshold: 20)
+    @discount2 = @edibles.bulk_discounts.create!(percentage: 50, threshold: 70)
+
+    visit merchant_discount_path(@edibles, @discount1)
+  end
+
+  it 'shows the quantity threshold and percentage discount' do 
+    expect(page).to have_content(@discount1.percentage)
+    expect(page).to have_content(@discount1.threshold)
+    expect(page).to_not have_content(@discount2.percentage)
+    expect(page).to_not have_content(@discount2.threshold)
+  end
+end
