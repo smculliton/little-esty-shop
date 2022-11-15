eca00b2195242a934fb5fac17148a0ba103d28aa
Sean Culliton <smculliton@gmail.com>
2022-11-14 19:04:22 -0700

    Remove prys

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 TO DO:
 
-To Get Threes
-- double check there are no MVC violations/ routes are restful
-- add sad path testing where applicaple
-
 To Get Fours
-- create partials
-- use before action helper methods in controllers
 - look into callbacks, scopes, application_helper view methods
 - refactor ActiveRecord methods to be cleaner
 - add edge case testing
--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,31 +1,29 @@
 require './app/poros/holiday_search'
 
 class MerchantDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new, :create, ]
+  before_action :find_merchant_and_discount, only: [:show, :destroy, :edit, :update]
+  
   def self.controller_path
     'merchants/discounts'
   end
 
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @holidays = HolidaySearch.create_holidays
   end
 
   def show
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:id])
   end
 
   def new 
-    @merchant = Merchant.find(params[:merchant_id])
     @discount = @merchant.bulk_discounts.new
     @holiday = params[:holiday]
   end
 
   def create 
-    @merchant = Merchant.find(params[:merchant_id])
-    discount = @merchant.bulk_discounts.new(discount_params)
+    @discount = @merchant.bulk_discounts.new(discount_params)
     
-    if discount.save
+    if @discount.save
       redirect_to merchant_discounts_path(@merchant)
     else
       flash[:notice] = 'Fields missing or invalid'
@@ -34,22 +32,15 @@ class MerchantDiscountsController < ApplicationController
   end
 
   def destroy
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:id])
     @discount.destroy
 
     redirect_to merchant_discounts_path(@merchant)
   end
 
   def edit
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:id])
   end 
 
   def update
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:id])
-    
     if @discount.update(discount_params)
       redirect_to merchant_discount_path(@merchant, @discount)
     else
@@ -61,5 +52,14 @@ class MerchantDiscountsController < ApplicationController
   private
   def discount_params
     params.require(:bulk_discount).permit(:percentage, :threshold, :name, :holiday)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_merchant_and_discount
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.find(params[:id])
   end
 end
